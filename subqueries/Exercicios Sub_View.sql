60a0331465ae189d6ad5a5f0ff774ca97c12fa75
vitorpossebontra@gmail.com <>
2023-10-27 10:29:28 -0300

    exercicios

---
+++ subqueries/Exercicios Sub_View.sql
@@ -0,0 +1,12 @@
+/*
+1. Crie uma visão que liste todos os funcionários que não são chefes.
+2. Faça uma visão que liste a quantidade de vendas que cada produto (o quanto
+cada produto foi vendido).
+3. Faça uma visão que liste os territórios e quantos vendedores estão
+vinculados a ele.
+4. Faça uma visão que retorne o nome do cliente da venda de maior valor.
+5. Faça uma visão que liste os vendedores ordenados pela lucratividade.
+6. Faça uma visão que retorne os produtos, seu fornecedor, sua categoria, seu
+preço e a informação de ele estar descontinuado ou não, para aqueles que
+possuem estoque.
+*/
